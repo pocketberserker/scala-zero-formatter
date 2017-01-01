042e34a868caf8c52d85d4448ceb80daa598dc72
pocketberserker <pocketberserker@gmail.com>
2017-01-01 18:54:59 +0900

    add serialize and deserialize methods for cats

--- cats-core/src/main/scala/zeroformatter/cats/package.scala
+++ cats-core/src/main/scala/zeroformatter/cats/package.scala
@@ -1,10 +1,21 @@
 package zeroformatter
 
+
+import _root_.cats.Eval
 import _root_.cats.functor._
 
 package object cats {
 
   implicit val formatterInvariantInstance: Invariant[Formatter] = new Invariant[Formatter] {
     def imap[A, B](fa: Formatter[A])(f: A => B)(g: B => A): Formatter[B] = fa.xmap(f, g)
+  }
+
+  object ZeroFormatter {
+
+    def serialize[T: Formatter](value: T): Array[Byte] =
+      zeroformatter.ZeroFormatter.serialize[T](value)
+
+    def deserialize[T: Formatter](bytes: Array[Byte]): Eval[T] =
+      Eval.later(zeroformatter.ZeroFormatter.deserialize[T](bytes))
   }
 }
