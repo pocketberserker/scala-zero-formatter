ae57c5e18f05aebed7dec78cb65e5185987fe499
pocketberserker <pocketberserker@gmail.com>
2017-01-17 04:55:12 +0900

    add IList formatter instance

--- scalaz/src/main/scala/zeroformatter/scalaz/package.scala
+++ scalaz/src/main/scala/zeroformatter/scalaz/package.scala
@@ -1,5 +1,6 @@
 package zeroformatter
 
+import java.nio.ByteBuffer
 import _root_.scalaz._
 import Formatter._
 
@@ -15,6 +16,42 @@ package object scalaz {
 
   implicit def maybeFormatter[T: Formatter]: Formatter[Maybe[T]] =
     Formatter[Option[T]].xmap(o => Maybe.fromOption(o), _.toOption)
+
+  implicit def ilistFormatter[A](implicit F: Formatter[A]): Formatter[IList[A]] = new Formatter[IList[A]] {
+    override val length = None
+
+    override def serialize(bytes: Array[Byte], offset: Int, value: IList[A]) =
+      if(value == null) intFormatter.serialize(bytes, offset, -1)
+      else {
+        val length = value.length
+        val bs =
+          F.length.map(l => BinaryUtil.ensureCapacity(bytes, offset, 4 + l * length))
+            .getOrElse(bytes)
+
+        value.foldLeft(intFormatter.serialize(bs, offset, length)){ case (acc, v) =>
+          val r = F.serialize(acc.value, offset + acc.byteSize, v)
+          LazyResult.strict(r.value, acc.byteSize + r.byteSize)
+        }
+      }
+
+    override def deserialize(buf: ByteBuffer, offset: Int) = {
+      val length = intFormatter.deserialize(buf, offset).value
+      if(length == -1) LazyResult(null, 4)
+      else if(length < -1) throw FormatException(offset, "Invalid List length.")
+      else {
+        var list: IList[A] = IList.empty
+        var byteSize = 4
+        var i = 0
+        while(i < length) {
+          val r = F.deserialize(buf, offset + byteSize)
+          list ::= r.value
+          byteSize += r.byteSize
+          i += 1
+        }
+        LazyResult.strict(list.reverse, byteSize)
+      }
+    }
+  }
 
   object ZeroFormatter {
 
---
+++ scalaz/src/test/scala/zeroformatter/scalaz/FormatterTest.scala
@@ -0,0 +1,22 @@
+package zeroformatter
+package scalaz
+
+import _root_.scalaz._
+import _root_.scalaz.std.anyVal._
+import _root_.scalaz.IList._
+import dog.TestCase
+import Formatter._
+
+object FormatterTest extends Base {
+
+  val `serialize and deserialize IList[Int]` = TestCase {
+    for {
+      values <- ArrayFormatterTest.`serialize Array[Int]`
+      value = IList.fromList(values._1.toList)
+      bytes = values._2
+      actualBytes = zeroformatter.ZeroFormatter.serialize(value)
+      _ <- assert.eq(bytes, actualBytes).lift
+      _ <- assert.eq(value, zeroformatter.ZeroFormatter.deserialize[IList[Int]](actualBytes)).lift
+    } yield ()
+  }
+}
