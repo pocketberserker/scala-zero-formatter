cd9ccb11c98cc182e12a4608b15afa89bad5b436
pocketberserker <pocketberserker@gmail.com>
2017-01-17 04:09:16 +0900

    cats-core 0.9.0

--- project/Build.scala
+++ project/Build.scala
@@ -76,7 +76,7 @@ object Build {
   lazy val catsCore = module("cats-core").settings(
     name := catsCoreName,
     libraryDependencies ++= Seq(
-      "org.typelevel" %% "cats-core" % "0.8.1"
+      "org.typelevel" %% "cats-core" % "0.9.0"
     )
   ).dependsOn(zeroFormatter)
 }
