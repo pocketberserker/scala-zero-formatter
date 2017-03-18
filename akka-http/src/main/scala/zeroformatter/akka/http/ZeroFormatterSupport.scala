3a5e9e9cd4469b8167c7981114b0d7b6029788b1
pocketberserker <pocketberserker@gmail.com>
2017-03-19 07:04:46 +0900

    abstract serialize and deserialize

--- akka-http/src/main/scala/zeroformatter/akka/http/ZeroFormatterSupport.scala
+++ akka-http/src/main/scala/zeroformatter/akka/http/ZeroFormatterSupport.scala
@@ -5,9 +5,16 @@ import akka.http.scaladsl.model.MediaTypes.`application/octet-stream`
 import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshaller}
 import zeroformatter._
 
-object ZeroFormatterSupport extends ZeroFormatterSupport
+object ZeroFormatterSupport extends ZeroFormatterSupport {
+  override def serialize[A: Formatter](value: A) = unsafe.ZeroFormatter.serialize(value)
+  override def deserialize[A: Formatter](bytes: Array[Byte]) = unsafe.ZeroFormatter.deserialize[A](bytes)
+}
 
 trait ZeroFormatterSupport {
+
+  def serialize[A: Formatter](value: A): Array[Byte]
+
+  def deserialize[A: Formatter](bytes: Array[Byte]): A
 
   private val zeroFormatterUnmarshaller =
     Unmarshaller.byteArrayUnmarshaller
@@ -21,10 +28,10 @@ trait ZeroFormatterSupport {
 
   implicit def unmarshaller[A: Formatter]: FromEntityUnmarshaller[A] = {
     zeroFormatterUnmarshaller
-      .map(bs => unsafe.ZeroFormatter.deserialize(bs))
+      .map(bs => deserialize(bs))
   }
 
   implicit def marshaller[A: Formatter]: ToEntityMarshaller[A] =
     zeroFormatterMarshaller
-      .compose(v => unsafe.ZeroFormatter.serialize(v))
+      .compose(v => serialize(v))
 }
